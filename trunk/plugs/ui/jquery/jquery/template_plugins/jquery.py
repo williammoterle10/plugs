bdbbbafb7fabe13b62fd241e0860cb7e032033f6
limodou <limodou@912515bb-fe1b-d127-1567-e02edd584596>
2011-04-21 07:07:33 +0000

    add missing files

--- trunk/plugs/ui/jquery/jquery/template_plugins/jquery.py
+++ trunk/plugs/ui/jquery/jquery/template_plugins/jquery.py
@@ -1,13 +1,20 @@
-def call(app, var, env, version='1.5.2', ui=False, theme='redmond', css_only=False, ui_version='1.8.11'):
+def call(app, var, env, version='1.5.2', ui=False, theme='redmond', css_only=False, ui_version='1.8.11', ajax_error=True):
     a = []
     if css_only:
         return {'toplinks':['jquery/ui/css/%s/jquery-ui-%s.custom.css' % (theme, ui_version)]}
     if version:
-        a.append('jquery/jquery-%s.js' % version)
+        a.append('jquery/jquery-%s.min.js' % version)
     if ui:
         a.append('jquery/ui/css/%s/jquery-ui-%s.custom.css' % (theme, ui_version))
-        a.append('jquery/jquery-%s.js' % version)
+        a.append('jquery/jquery-%s.min.js' % version)
         a.append('jquery/ui/js/jquery-ui-%s.custom.min.js' % ui_version)
         a.append('jquery/ui/js/jquery.ui.datepicker.zh.js')
+        
+    if ajax_error:
+        a.append("""<script>$(function(){
+    $(document).ajaxError(function(e, xhr, settings, exception) { 
+        alert('error in: ' + settings.url + ' \\n'+'error:\\n' + 'ajax request process error!' ); 
+    });
+}); </script>""")
         
     return {'toplinks':a}
